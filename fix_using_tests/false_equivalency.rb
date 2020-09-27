64947f66cf21875cd279d7ecb6647e643918abbb
Dena Neuman <denaweiss5@gmail.com>
2020-09-27 13:37:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,8 +1,8 @@
 def selection(num)
-  if num = 1
+  if num == 1
     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
+    binding.pry
     "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
 end
