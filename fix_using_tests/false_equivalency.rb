db122d6f6d5844274b632b6924c1b9dfc889e0dc
Dena Neuman <denaweiss5@gmail.com>
2020-09-27 13:45:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,9 +1,10 @@
 
 def selection(num)
-
-  if num = 1
+num = 1
+  if num == 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+    num = 2
+  elsif num == 2
     puts "HAM HAM HAM IN MY TUMMY"
   end
 end
